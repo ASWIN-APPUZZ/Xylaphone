e01c222275b4af35948a00b3d5520a12b845de8c
aswin <aswin0484@gmail.com>
2023-10-16 16:38:29 +0530

    Xylaphone Notes on button IOS

--- Xylaphone/ViewController.swift
+++ Xylaphone/ViewController.swift
@@ -1,19 +1,25 @@
-//
-//  ViewController.swift
-//  Xylaphone
-//
-//  Created by ASWIN A on 16/10/23.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let arr =
+    ["note1","note2","note3","note4","note5","note6","note7",]
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func xylo(_ sender: UIButton) {
+        playSound(tag: sender.tag)
+        
+    }
+    func playSound(tag: Int ){
+        let path = Bundle.main.url(forResource: arr[tag], withExtension: ".wav")
+        audioPlayer=try!AVAudioPlayer(contentsOf: path!)
+        audioPlayer.play()
     }
 
-
 }
-
